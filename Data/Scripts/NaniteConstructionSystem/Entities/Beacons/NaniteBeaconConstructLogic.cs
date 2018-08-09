f6d6ed6b26b68ea7416809c7e286d23d83d4c06a
Florian Kinder <florian.kinder@fankserver.com>
2018-08-09 18:22:24 +0200

    Revert "Test"
    This reverts commit cfb8a3609a4e9ab4f3d430cc256395e06448db20.

--- Data/Scripts/NaniteConstructionSystem/Entities/Beacons/NaniteBeaconConstructLogic.cs
+++ Data/Scripts/NaniteConstructionSystem/Entities/Beacons/NaniteBeaconConstructLogic.cs
@@ -3,8 +3,6 @@ using VRage.Game.Components;
 using VRage.ObjectBuilders;
 using Sandbox.ModAPI;
 
-using NaniteConstructionSystem.Extensions;
-
 namespace NaniteConstructionSystem.Entities.Beacons
 {
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_OxygenFarm), false, "LargeNaniteBeaconConstruct", "SmallNaniteBeaconConstruct")]
@@ -25,11 +23,6 @@ namespace NaniteConstructionSystem.Entities.Beacons
 
             Logging.Instance.WriteLine($"ADDING Repair Beacon: {Entity.EntityId}");
             m_beacon = new NaniteBeaconConstruct((IMyTerminalBlock)Entity);
-
-            if (Sync.IsClient)
-            {
-                NaniteConstructionManager.NaniteSync.SendNeedBeaconTerminalSettings(Entity.EntityId);
-            }
         }
 
         public override void Close()
