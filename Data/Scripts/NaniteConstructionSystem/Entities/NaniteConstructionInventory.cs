82c73b19b1aae45f1808bc72fd8daa4e4ba07607
SplenectomY <skaterjohn16@hotmail.com>
2018-10-20 21:22:57 -0400

    Add check to stop KeyNotFoundException()
    Even though the catch{} keeps the error from doing any harm, it's annoying in the console, so let's do a proper check for it before it gets that far.

--- Data/Scripts/NaniteConstructionSystem/Entities/NaniteConstructionInventory.cs
+++ Data/Scripts/NaniteConstructionSystem/Entities/NaniteConstructionInventory.cs
@@ -78,7 +78,8 @@ namespace NaniteConstructionSystem.Entities
                                 {
                                     inventory.RemoveItemsOfType((int)amount, (MyObjectBuilder_PhysicalObject)MyObjectBuilderSerializer.CreateNewObject(typeof(MyObjectBuilder_Component), componentNeeded.Key));
                                     constructionInventory.AddItems((int)amount, (MyObjectBuilder_PhysicalObject)MyObjectBuilderSerializer.CreateNewObject(typeof(MyObjectBuilder_Component), componentNeeded.Key));
-                                    ComponentsRequired[componentNeeded.Key] -= (int)amount;
+                                    if (ComponentsRequired.ToList().ContainsKey(componentNeeded.Key)) 
+                                        ComponentsRequired[componentNeeded.Key] -= (int)amount;
                                 }
                                 catch (Exception ex)
                                 {
