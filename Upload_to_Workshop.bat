8b7b82fb12f12e0e825a4a062458c1e3d1d95a17
Bryon <30876973+nukeguard@users.noreply.github.com>
2020-05-06 19:49:58 -0400

    Create Upload_to_Workshop.bat

---
+++ Upload_to_Workshop.bat
@@ -0,0 +1,4 @@
+set SEInstallDir="D:\games\Steam\steamapps\common\SpaceEngineers"
+for %%I in (.) do set ParentDirName=%%~nxI
+%SEInstallDir%\Bin64\SEWorkshopTool.exe --upload --compile --mods "%ParentDirName%" --exclude .bat .psd .fbx .hkt .xml .blend .blend1
+pause
